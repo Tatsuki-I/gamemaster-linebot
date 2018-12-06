6efba354db565c39f6360faae284b2c6f3b8392f
Tatsuki <tatsuki@konokaze.jp>
2018-12-06 22:25:25 +0900

    Update

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-handler = YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+line_bot_api = LineBotApi["YOUR_CHANNEL_ACCESS_TOKEN"]
+handler = WebhookHandler["YOUR_CHANNEL_SECRET"]
 
 
 @app.route("/callback", methods=['POST'])
