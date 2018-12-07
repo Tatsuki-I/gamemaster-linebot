b522ac56c17e21d5854ac7fd85a29c4751e2fdbb
Tatsuki <tatsuki@konokaze.jp>
2018-12-07 11:42:00 +0900

    update

--- main.py
+++ main.py
@@ -42,16 +42,16 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
 def start_werewolf(event):
     if event.message.text == "werewolf":
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="人狼ゲームを始めます"))
+
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
