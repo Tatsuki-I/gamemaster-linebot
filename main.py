ce29cf76c326f92f20f32d6a3a68bf410559c23a
Tatsuki <tatsuki@konokaze.jp>
2018-12-07 17:03:53 +0900

    update

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def werewolf_start(event):
             random.shuffle(jobs)
             for (uid, job) in zip(werewolf.user_id, jobs):
                 line_bot_api.push_message(uid, TextSendMessage(text=job))
-    elif event.message.text == "/end":
+    elif not werewolf.phase == "wait" and vent.message.text == "/end":
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text = "ゲームを強制終了します。"))
