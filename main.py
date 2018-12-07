d0f0b487689272121d9d8caf61c0f603f39301e5
Tatsuki <tatsuki@konokaze.jp>
2018-12-07 17:01:39 +0900

    update

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def werewolf_start(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text = "受け付けを締め切りました。"))
-　　　　　　　jobs = jobs2
+            jobs = jobs2
             #random.shuffle(jobs)
             for (uid, job) in zip(werewolf.user_id, jobs):
                 line_bot_api.push_message(uid, TextSendMessage(text=job))
